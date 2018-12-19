c27117b972a70bc55e155833fdd3ea5b3093d5b2
Joel Palmer <joelpalmer@gmail.com>
2018-12-19 10:33:53 -0600

    basic structure of clu

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 "use strict";
 const fs = require('fs');
 const request = require('request');
+const program = require('commander');
+const pkg = require('./package.json');
+
+program
+    .version(pkg.version)
+    .description(pkg.description)
+    .usage('[options] <command> [...]')
+    .option('-o, --host, <hostname>', 'hostname [localhost]', 'localhost')
+    .option('-p, --port <number>', 'port number [9200]', '9200')
+    .option('-j, --json', 'format output as JSON')
+    .option('-i, --index <name>', 'which index to use')
+    .option('-t, --type <type>', 'default type for bulk operations');
+
+program.parse(process.argv);
+
+if (!program.args.filter(arg => typeof arg === 'object').length) {
+    program.help();
+};
