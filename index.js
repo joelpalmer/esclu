0eda29cf405a28e481218a46461671f9153b0d32
Joel Palmer <joelpalmer@gmail.com>
2018-12-19 10:53:35 -0600

    add fullUrl function

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 "use strict";
-const fs = require('fs');
-const request = require('request');
-const program = require('commander');
-const pkg = require('./package.json');
+const fs = require("fs");
+const request = require("request");
+const program = require("commander");
+const pkg = require("./package.json");
+
+const fullUrl = (path = '') => {
+    let url = `http://${program.host}:${program.port}/`;
+    if (program.index) {
+        url += program.index + '/';
+        if (program.type) {
+            url += program.type + '/';
+        }
+    }
+    return url + path.replace(/^\/*/,'');
+};
 
 program
-    .version(pkg.version)
-    .description(pkg.description)
-    .usage('[options] <command> [...]')
-    .option('-o, --host, <hostname>', 'hostname [localhost]', 'localhost')
-    .option('-p, --port <number>', 'port number [9200]', '9200')
-    .option('-j, --json', 'format output as JSON')
-    .option('-i, --index <name>', 'which index to use')
-    .option('-t, --type <type>', 'default type for bulk operations');
+  .version(pkg.version)
+  .description(pkg.description)
+  .usage("[options] <command> [...]")
+  .option("-o, --host, <hostname>", "hostname [localhost]", "localhost")
+  .option("-p, --port <number>", "port number [9200]", "9200")
+  .option("-j, --json", "format output as JSON")
+  .option("-i, --index <name>", "which index to use")
+  .option("-t, --type <type>", "default type for bulk operations");
 
 program.parse(process.argv);
 
-if (!program.args.filter(arg => typeof arg === 'object').length) {
-    program.help();
-};
+if (!program.args.filter(arg => typeof arg === "object").length) {
+  program.help();
+}
